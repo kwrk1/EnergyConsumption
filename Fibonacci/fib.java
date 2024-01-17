c81a08590e9a49fb82b28229e2081c78a616d973
Kai <kai.werk@gmx.de>
2024-01-17 15:32:08 +0100

    fib

---
+++ Fibonacci/fib.java
@@ -0,0 +1,16 @@
+package Fibonacci;
+
+public class fib {
+
+    public int fibonacci(int n) {
+        if(n<=1){
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);        
+    }
+	public static void main(String[] args){
+        
+        fib fib = new fib();
+		System.out.println(fib.fibonacci(6));	
+	}
+}
---
+++ Fibonacci/fib.py
@@ -0,0 +1,12 @@
+def fib(n: int) -> int:
+    if (n<=1):
+        return n
+    
+    return fib(n-1) + fib(n-2)
+
+
+def main():
+    print(fib(6))
+
+if __name__ == "__main__":
+    main()
